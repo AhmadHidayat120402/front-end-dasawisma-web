7ab87a8afa35b34e188b5cca4e6ad4b8f8c914e9
Ahmad Hidayat <ahmadhidayat120402@gmail.com>
2024-01-17 10:16:22 +0700

    project front-end dasawisma

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="images/logo_dasawisma.png">
+    <link rel="stylesheet" href="vendors/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles/style.css">
+    <title>DASAWISMA</title>
+</head>
+
+<body class="text-center">
+    <main class="fixed-bottom mx-auto text-center">
+        <h4 class="content-judul">DASAWISMA</h4>
+        <h5 class="content-subjudul mb-5">TP-PKK Kota Makassar</h5>
+        <p class="content-paragraf">DASAWISMA adalah sistem informasi yang membantu dalam <br> pendataan dan pengelolaan DASAWISMA di kota Makassar.</p>
+        <div class="d-flex justify-content-center mb-5">
+            <button type="button" class="btn btn-warning btn-lg">Masuk Aplikasi</button>
+            <button type="button" class="btn btn-primary btn-lg">Lihat Rekap</button>
+        </div>
+    </main>
+    <script src="vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
